379ef5ae1ff15dc40bcfc35a305d2800fdee8241
wtaype <wtaype@users.noreply.github.com>
2026-01-23 05:39:07 +0000

    deploy: f0e265e0bcc8428c4ec5e597986abacebca00ff5

---
+++ v11/assets/smile-L6iSMh65.js
@@ -0,0 +1,14 @@
+import{j as t}from"./vendor-gzd0YkcT.js";import{d as f}from"./firebase-Dt3YUmeU.js";import{g as p,d,T as l,s as v,a as h}from"./firebase-BY8gY55M.js";import{g as r,s as u,a as m,N as o,b,S as g}from"./main-BSYQa8nq.js";import"./main-BqyDT-8R.js";let e=r("wiSmile");const i="wiNotas",k=async()=>{const c=r(i),a=c?.fechaUpdate?m(l).get(c.fechaUpdate,"DD/MM/YYYY HH:mm"):"Sin notas";return`
+   <div class="miweb">
+    <div class="mhead"><h1 class="mh1"><i class="fas fa-sticky-note"></i> ${g()} ${e.nombre}</h1></div>
+    <div class="mibody">
+     <textarea class="nota-text" placeholder="Empieza escribir tus notas">${c?.notas||""}</textarea>
+     <div class="nota-footer">
+      <span class="nota-fecha"><i class="far fa-clock"></i> ${c?.fechaCreacion?"Actualizado":"Nota creada"}: ${a}</span>
+      <div class="nota-btns">
+       <button class="btn btn-save"><i class="fas fa-save"></i> Guardar</button>
+       <button class="btn btn-delete"><i class="fas fa-trash"></i> Eliminar</button>
+      </div>
+     </div>
+    </div>
+   </div>`},C=async()=>{if(!r(i))try{const a=await p(d(f,"misnotas",e.usuario));if(a.exists()){const s=a.data();t(".nota-text").val(s.notas),u(i,s),t(".nota-fecha").html(`<i class="far fa-clock"></i> Actualizado: ${m(l).get(s.fechaUpdate,"DD/MM/YYYY HH:mm")}`),o("✅ Notas cargadas","success")}}catch(a){console.error(a),o("❌ Error al cargar","error")}t(".btn-save").click(async function(){const a=t(".nota-text").val().trim();if(!a)return o("⚠️ Escribe algo primero","warning");b(this,!0);try{const s=r(i);await v(d(f,"misnotas",e.usuario),{email:e.email,usuario:e.nombre,notas:a,fechaUpdate:h(),...!s?.fechaCreacion&&{fechaCreacion:h()}},{merge:!0});const n=Date.now();u(i,{email:e.email,usuario:e.nombre,notas:a,fechaUpdate:n,fechaCreacion:s?.fechaCreacion||n}),t(".nota-fecha").html(`<i class="far fa-clock"></i> Actualizado: ${m(l).get(n,"DD/MM/YYYY HH:mm")}`),o("✅ Guardado exitosamente!","success")}catch(s){console.error(s),o("❌ Error al guardar","error")}finally{b(this,!1)}}),t(".btn-delete").click(function(){confirm("¿Eliminar todas las notas?")&&(t(".nota-text").val(""),t(".nota-fecha").html('<i class="far fa-clock"></i> Nota creada: Sin notas'),o("🗑️ Notas eliminadas","info"))})},E=()=>{console.log("😊 Smile limpiado")};export{E as cleanup,C as init,k as render,e as smile};
